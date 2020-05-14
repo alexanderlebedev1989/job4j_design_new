1d103a3e1dec3d16c963fc398cf94a6cec922f7e
alexanderlebedev1989 <AlexanderLebedev1989@gmail.com>
2020-05-14 20:18:43 +0500

    1. Читаем файл конфигурации [#271925]

--- chapter_002/src/main/java/ru/job4j/io/Config.java
+++ chapter_002/src/main/java/ru/job4j/io/Config.java
@@ -1,41 +1,56 @@
-//package ru.job4j.io;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//import java.io.BufferedReader;
-//import java.io.FileReader;
-//import java.util.StringJoiner;
-//
-//public class Config {
-//    private final String path;
-//    private final Map<String, String> values = new HashMap<String, String>();
-//
-//    public Config(final String path) {
-//        this.path = path;
-//    }
-//
-//    public void load() {
-//        try (BufferedReader read = new BufferedReader(new FileReader(path))) {
-//            read.lines().forEach();
-//        }
-//    }
-//
-//    public String value(String key) {
-//        throw new UnsupportedOperationException("Don't impl this method yet!");
-//    }
-//
-//    @Override
-//    public String toString() {
-//        StringJoiner out = new StringJoiner(System.lineSeparator());
-//        try (BufferedReader read = new BufferedReader(new FileReader(path))) {
-//            read.lines().forEach(out::add);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return out.toString();
-//    }
-//
-//    public static void main(String[] args) {
-//        System.out.println(new Config("app.properties"));
-//    }
-//}
+package ru.job4j.io;
+
+import java.io.File;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<String, String>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(path))) {
+            List<String> list = new ArrayList<>();
+            read.lines().filter(s -> s.contains("=")).forEach(list::add);
+            for (String s : list) {
+                 String[] strings = s.split("=");
+                 values.put(strings[0], strings[1]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String value(String key) {
+        if (!values.containsKey(key)) {
+            throw new UnsupportedOperationException("Don't impl this method yet!");
+        }
+        String s = values.get(key);
+        return s;
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(path))) {
+            read.lines().filter(s -> s.contains("=")).forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        Config config1 = new Config("./data/pair_without_comment.properties");
+        Config config2 = new Config("./data/pair_with_comment.properties");
+        System.out.println(config1.toString());
+        System.out.println();
+        System.out.println(config2.toString());
+    }
+}
