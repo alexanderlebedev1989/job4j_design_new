e3b4718321eaa749b47a3b962ce409873e8cfb52
alexanderlebedev1989 <AlexanderLebedev1989@gmail.com>
2020-05-18 14:22:45 +0500

    3.0. Тестирование IO [#271919]

--- chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,22 +1,34 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.List;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class AnalizyTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     @Test
-    public void test() {
+    public void test() throws IOException {
         List<String> list = List.of("Начало периода: 10:58:01, конец периода: 10:59:01",
                                     "Начало периода: 11:01:02, конец периода: 11:02:02");
         Analizy analizy = new Analizy();
-        String source = "./src/test/resources/server.log";
-        String target = "./src/test/resources/unavailable.csv";
-        analizy.unavailable(source, target);
+        File source =  folder.newFile("server.log");
+        File target = folder.newFile("unavailable.csv");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println("200 10:57:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:01");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
+        }
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
         try (BufferedReader read = new BufferedReader(new FileReader(target))) {
             String line;
             int index = 0;
