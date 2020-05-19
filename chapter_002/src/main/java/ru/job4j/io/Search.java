2ac3013851dae88bcb7817fd3da993404bc405da
alexanderlebedev1989 <AlexanderLebedev1989@gmail.com>
2020-05-19 18:49:16 +0500

    5. Валидация параметров запуска. [#271920]

--- chapter_002/src/main/java/ru/job4j/io/Search.java
+++ chapter_002/src/main/java/ru/job4j/io/Search.java
@@ -7,8 +7,11 @@ import java.util.stream.Collectors;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, "js").forEach(System.out::println);
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT Folder.");
+        }
+        Path start = Paths.get(args[0]);
+        search(start, args[1]).forEach(System.out::println);
     }
     public static List<Path> search(Path root, String ext) throws IOException {
         SearchFiles searcher = new SearchFiles();
