7bc4fc41a12eb63bd564f5a66ecf1783e4c3ec6b
alexanderlebedev1989 <AlexanderLebedev1989@gmail.com>
2020-05-26 13:47:45 +0500

    5.2. Архивировать проект. [#271927]

--- chapter_002/src/main/java/ru/job4j/io/ArgZip.java
+++ chapter_002/src/main/java/ru/job4j/io/ArgZip.java
@@ -6,21 +6,51 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static java.nio.file.FileVisitResult.CONTINUE;
 
 public class ArgZip extends SimpleFileVisitor<Path> {
-    private List<File> files = new ArrayList<>();
+
+    private final String[] args;
+    private final List<File> files = new ArrayList<>();
+    private final Map<String, String> values = new HashMap<>();
+
+    public ArgZip(String[] args) {
+        this.args = args;
+    }
+
+    public String directory() {
+        return values.get("d");
+    }
+
+    public String exclude() {
+        return values.get("e");
+    }
+
+    public String output() {
+        return values.get("o");
+    }
 
     public List<File> getFiles() {
         return files;
     }
 
-   @Override
-   public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-       files.add(file.toFile());
-       return CONTINUE;
-   }
+    public void parse(String[] args) {
+        if (args.length != 3) {
+            throw new IllegalArgumentException();
+        }
+        Arrays.stream(args)
+                .map(s -> s.split("="))
+                .forEach(strings -> values.put(strings[0].substring(1), strings[1]));
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        String endOfWord = exclude();
+        if (!file.toString().endsWith(endOfWord)) files.add(file.toFile());
+        return CONTINUE;
+    }
 }
+
+
--- chapter_002/src/main/java/ru/job4j/io/Zip.java
+++ chapter_002/src/main/java/ru/job4j/io/Zip.java
@@ -2,12 +2,14 @@ package ru.job4j.io;
 
 import java.io.*;;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
+
     public void packFiles(List<File> sources, File target) {
         try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(target))) {
             for (File f : sources) {
@@ -32,13 +34,24 @@ public class Zip {
         }
     }
 
+    public List<File> seekBy(ArgZip args) throws IOException {
+        Path folder = Paths.get(args.directory());
+        Files.walkFileTree(folder, args);
+        return args.getFiles();
+    }
+
     public static void main(String[] args) throws IOException {
         new Zip().packSingleFile(
                 new File("./chapter_002/pom.xml"),
                 new File("./chapter_002/pom.zip")
         );
-        ArgZip argZip = new ArgZip();
-        Files.walkFileTree(Paths.get(args[0]), argZip);
-        new Zip().packFiles(argZip.getFiles(), new File(args[1]));
+
+        ArgZip argZip = new ArgZip(new String[]{args[0], args[1], args[2]});
+        argZip.parse(args);
+
+        Zip zip = new Zip();
+        List<File> files = zip.seekBy(argZip);
+        File folder = new File(argZip.output());
+        zip.packFiles(files, folder);
     }
 }
