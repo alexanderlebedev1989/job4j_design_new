0f327b95557a5fd4c6ecdc63ea15b05da873df4f
alexanderlebedev1989 <AlexanderLebedev1989@gmail.com>
2020-06-17 14:03:28 +0500

    фильтры

--- chapter_003/src/main/java/ru/job4j_design_new/create/filters.sql
+++ chapter_003/src/main/java/ru/job4j_design_new/create/filters.sql
@@ -2,10 +2,11 @@
 SELECT prod_name, type_id FROM products WHERE type_id = (SELECT type_id FROM type WHERE type_name = 'СЫР');
 
 2. Написать запрос получения всех продуктов, у кого в имени есть слово "мороженое"
-SELECT prod_name FROM products WHERE prod_name LIKE '%мороженое';
+SELECT prod_name FROM products WHERE prod_name LIKE '%мороженое%';
 
 3. Написать запрос, который выводит все продукты, срок годности которых заканчивается в следующем месяце.
-SELECT prod_name, expired_date FROM products WHERE expired_date > '2020-06-30' AND expired_date < '2020-08-25';
+SELECT prod_name, expired_date FROM products WHERE expired_date
+    BETWEEN date '2020-07-01' AND date '2020-07-01' + interval '30 day';
 
 4. Написать запрос, который выводит самый дорогой продукт.
 SELECT prod_name, price FROM products WHERE price = (SELECT MAX(price) FROM products);
@@ -15,8 +16,7 @@ SELECT SUM(count) FROM products WHERE type_id = (SELECT type_id FROM type WHERE 
 
 6. Написать запрос получение всех продуктов с типом "СЫР" и "МОЛОКО".
  SELECT prod_name FROM products WHERE type_id IN (
-    (SELECT type_id FROM type WHERE type_name = 'СЫР'),
-    (SELECT type_id FROM type WHERE type_name = 'МОЛОКО')
+    SELECT type_id FROM type WHERE type_name = 'СЫР' OR type_name = 'МОЛОКО'
 );
 
 7. Написать запрос, который выводит тип продуктов, которых осталось меньше 10 штук.
